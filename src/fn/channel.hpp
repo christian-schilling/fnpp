e40ce39f31461da7178a4af00c03ece590fca72b
Christian Schilling <initcrash@gmail.com>
2015-06-08 21:46:24 +0200

    channel: working, no thread safety

---
+++ src/fn/channel.hpp
@@ -0,0 +1,173 @@
+#ifndef _4fac367e_60d1_4c71_8690_910625d21ebd
+#define _4fac367e_60d1_4c71_8690_910625d21ebd
+
+
+namespace fn{
+
+namespace fn_ {
+
+template<typename T, size_t N_>
+class Ring
+{
+    auto static const N = N_ + 1;
+
+    size_t p0 = 0;
+    size_t p1 = 0;
+
+    struct T_mem { uint8_t mem[sizeof(T)]; };
+    T_mem data[N];
+
+public:
+
+    Ring(Ring const&) = delete;
+
+    Ring() {}
+
+    Ring(Ring&& o)
+    {
+        T* tmp;
+        while((tmp = o.pop())){
+            new (push()) T(fn_::move(*tmp));
+            tmp->~T();
+        }
+    };
+
+    bool empty() const { return p0 == p1; }
+    bool full() const { return (p0+1) % N == p1; }
+
+    T* push()
+    {
+        if(!full()){
+            auto const p = p0;
+            p0 = (p0 + 1) % N;
+            return reinterpret_cast<T*>(data+p);
+        }
+        return nullptr;
+    }
+
+    T* pop()
+    {
+        if(!empty()){
+            auto const p = p1;
+            p1 = (p1 + 1) % N;
+            return reinterpret_cast<T*>(data+p);
+        }
+        return nullptr;
+    }
+
+    template<typename F>
+    void remove_if(F f)
+    {
+        auto from = p1;
+        auto to = p1;
+
+        while(from < p0){
+            if(from != to){
+                reinterpret_cast<T*>(data+to)->~T();
+                new (data+to) T(fn_::move(*reinterpret_cast<T*>(data+from)));
+                reinterpret_cast<T*>(data+from)->~T();
+            }
+            if(!f(*reinterpret_cast<T*>(data+from))){
+                to = (to + 1) % N;
+            }
+            from = (from + 1) % N;
+        }
+        p0 = to;
+    }
+};
+
+}
+
+template<typename T, size_t N>
+struct Channel
+{
+    class Sender;
+
+    class Receiver
+    {
+        friend struct Channel;
+
+        Receiver() {}
+
+        optional<Sender&> tx;
+
+    public:
+
+        Receiver(Receiver const&) = delete;
+
+        Receiver(Receiver&& o):
+            tx(fn_::move(o.tx)),
+            queue(fn_::move(o.queue))
+        {
+            tx >>[&](Sender& tx){
+                tx.rx = *this;
+            };
+        }
+
+
+        optional<T> recv()
+        {
+            auto const p = queue.pop();
+            if(p){
+                optional<T> tmp(fn_::move(*p));
+                p->~T();
+                return tmp;
+            }
+            return {};
+        }
+
+        template<typename F>
+        void remove_if(F f)
+        {
+            queue.remove_if(f);
+        }
+
+        fn_::Ring<T,N> queue;
+    };
+
+    class Sender
+    {
+        friend struct Channel;
+
+        optional<Receiver&> rx;
+
+        Sender(Receiver& rx):
+            rx(rx)
+        {
+            rx.tx = *this;
+        }
+
+    public:
+
+        Sender(Sender const&) = delete;
+
+        Sender(Sender&& o):
+            rx(fn_::move(o.rx))
+        {
+            o.rx = {};
+        }
+
+        bool send(T v)
+        {
+            return rx >>[&](Receiver& rx){
+                auto const p = rx.queue.push();
+                if(p){
+                    new (p) T(fn_::move(v));
+                    return true;
+                }
+                return false;
+            } | false;
+        }
+    };
+
+    Channel():
+        tx(rx)
+    {}
+
+    Receiver rx;
+    Sender tx;
+};
+
+}
+
+#endif
---
+++ tests/channel.cpp
@@ -0,0 +1,231 @@
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <utility>
+#include "catch.hpp"
+
+#include <fn++.hpp>
+#include <fn/channel.hpp>
+using namespace fn;
+
+TEST_CASE("Channel [int]")
+{
+    auto channel = Channel<int,3>();
+    REQUIRE_FALSE(channel.rx.recv().valid());
+
+    SECTION("send one message")
+    {
+        channel.tx.send(123);
+        CHECK(123 == ~channel.rx.recv());
+        REQUIRE_FALSE(channel.rx.recv().valid());
+    }
+
+    SECTION("send two messages")
+    {
+        CHECK(channel.tx.send(123));
+        CHECK(channel.tx.send(124));
+        CHECK(123 == ~channel.rx.recv());
+        CHECK(124 == ~channel.rx.recv());
+        CHECK_FALSE(channel.rx.recv().valid());
+    }
+
+    SECTION("send more messages than fit")
+    {
+        CHECK(channel.tx.send(123));
+        CHECK(channel.tx.send(124));
+        CHECK(channel.tx.send(125));
+        CHECK_FALSE(channel.tx.send(126));
+        CHECK(123 == ~channel.rx.recv());
+        CHECK(124 == ~channel.rx.recv());
+        CHECK(125 == ~channel.rx.recv());
+        CHECK_FALSE(channel.rx.recv().valid());
+    }
+
+    SECTION("move sender")
+    {
+        auto tx = std::move(channel.tx);
+
+        CHECK_FALSE(channel.tx.send(2));
+        CHECK(tx.send(1));
+
+        CHECK(1 == ~channel.rx.recv());
+        CHECK_FALSE(channel.rx.recv().valid());
+    }
+
+    SECTION("move receiver, then send")
+    {
+        auto rx = std::move(channel.rx);
+
+        CHECK(channel.tx.send(1));
+
+        CHECK_FALSE(channel.rx.recv().valid());
+
+        CHECK(1 == ~rx.recv());
+        CHECK_FALSE(rx.recv().valid());
+    }
+
+    SECTION("send, then move receiver")
+    {
+        CHECK(channel.tx.send(1));
+        auto rx = std::move(channel.rx);
+
+        CHECK_FALSE(channel.rx.recv().valid());
+
+        CHECK(1 == ~rx.recv());
+        CHECK_FALSE(rx.recv().valid());
+    }
+
+    SECTION("remove elements")
+    {
+        CHECK(channel.tx.send(1));
+        CHECK(channel.tx.send(2));
+        CHECK(channel.tx.send(3));
+
+        channel.rx.remove_if([](int const& i) -> bool {
+            return i % 2;
+        });
+
+        CHECK(2 == ~channel.rx.recv());
+        CHECK_FALSE(channel.rx.recv().valid());
+    }
+}
+
+TEST_CASE("Channel [unique_ptr]")
+{
+    auto channel = Channel<std::unique_ptr<int>,3>();
+    REQUIRE_FALSE(channel.rx.recv().valid());
+
+    SECTION("send one message")
+    {
+        auto up = std::unique_ptr<int>(new int);
+        *up = 1234;
+
+        channel.tx.send(std::move(up));
+        CHECK(nullptr == up);
+
+        auto r = channel.rx.recv();
+        CHECK(r.valid());
+        r >>[&](std::unique_ptr<int>& i){
+            REQUIRE(i != nullptr);
+            CHECK(1234 == *i);
+        };
+
+        REQUIRE_FALSE(channel.rx.recv().valid());
+    }
+
+    SECTION("move receiver, then send")
+    {
+        auto rx = std::move(channel.rx);
+
+        CHECK(channel.tx.send(std::unique_ptr<int>(new int(1))));
+
+        CHECK_FALSE(channel.rx.recv().valid());
+
+        CHECK(rx.recv().valid());
+        CHECK_FALSE(rx.recv().valid());
+    }
+
+    SECTION("send, then move receiver")
+    {
+        CHECK(channel.tx.send(std::unique_ptr<int>(new int(1))));
+        auto rx = std::move(channel.rx);
+
+        CHECK_FALSE(channel.rx.recv().valid());
+
+        CHECK(rx.recv().valid());
+        CHECK_FALSE(rx.recv().valid());
+    }
+}
+
+TEST_CASE("Ring")
+{
+    fn_::Ring<int,3> queue;
+    CHECK_FALSE(queue.pop());
+    CHECK(queue.empty());
+
+    SECTION("push once")
+    {
+        new (queue.push()) int(7);
+        CHECK_FALSE(queue.empty());
+        CHECK(7 == *queue.pop());
+        CHECK_FALSE(queue.pop());
+    }
+
+    SECTION("push twice")
+    {
+        new (queue.push()) int(6);
+        CHECK_FALSE(queue.empty());
+        new (queue.push()) int(8);
+        CHECK_FALSE(queue.empty());
+        CHECK(6 == *queue.pop());
+        CHECK_FALSE(queue.empty());
+        CHECK(8 == *queue.pop());
+        CHECK(queue.empty());
+    }
+
+    SECTION("push twice, twice")
+    {
+        new (queue.push()) int(6);
+        new (queue.push()) int(8);
+        CHECK(6 == *queue.pop());
+        CHECK(8 == *queue.pop());
+
+        new (queue.push()) int(2);
+        new (queue.push()) int(3);
+        CHECK(2 == *queue.pop());
+        CHECK(3 == *queue.pop());
+    }
+
+    SECTION("overflow")
+    {
+        new (queue.push()) int(1);
+        new (queue.push()) int(2);
+        new (queue.push()) int(3);
+        CHECK_FALSE(queue.push());
+        CHECK_FALSE(queue.push());
+        CHECK_FALSE(queue.push());
+        CHECK(1 == *queue.pop());
+        CHECK(2 == *queue.pop());
+        CHECK(3 == *queue.pop());
+        CHECK(queue.empty());
+    }
+
+};
+
+TEST_CASE("Ring remove_if")
+{
+    fn_::Ring<int,5> queue;
+    CHECK(queue.empty());
+
+    new (queue.push()) int(1);
+    new (queue.push()) int(2);
+    new (queue.push()) int(3);
+    new (queue.push()) int(4);
+
+    SECTION("remove none")
+    {
+        queue.remove_if([](int const&) -> bool { return false; });
+
+        CHECK(1 == *queue.pop());
+        CHECK(2 == *queue.pop());
+        CHECK(3 == *queue.pop());
+        CHECK(4 == *queue.pop());
+        CHECK(queue.empty());
+    }
+
+    SECTION("remove all")
+    {
+        queue.remove_if([](int const&) -> bool { return true; });
+
+        CHECK(queue.empty());
+    }
+
+    SECTION("remove uneven")
+    {
+        queue.remove_if([](int const& x) -> bool { return x % 2; });
+
+        CHECK(2 == *queue.pop());
+        CHECK(4 == *queue.pop());
+        CHECK(queue.empty());
+    }
+}
